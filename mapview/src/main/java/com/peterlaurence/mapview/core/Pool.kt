8aca1e025fbb7d7663c7628d5e0b23128eec8547
plr <plr.devs@gmail.com>
2020-11-08 19:47:49 +0100

    Use ArrayDeque instead of LinkedList

--- mapview/src/main/java/com/peterlaurence/mapview/core/Pool.kt
+++ mapview/src/main/java/com/peterlaurence/mapview/core/Pool.kt
@@ -1,17 +1,18 @@
 package com.peterlaurence.mapview.core
 
-import java.util.*
+import kotlin.collections.ArrayDeque
 
 internal class Pool<T>(private val threshold: Int = 100) {
     var size: Int = 0
         private set
 
-    private val pool = LinkedList<T>()
+    private val pool = ArrayDeque<T>()
 
     fun get(): T? {
         return if (pool.isNotEmpty()) {
-            size--
-            pool.removeFirst()
+            pool.removeFirstOrNull().also {
+                if (it != null) size--
+            }
         } else {
             null
         }
